e0b6fb0dc5fe44db8c8507008eb00fbdc1b2ef0b
harshsinghreal <93440683+harshsinghreal@users.noreply.github.com>
2022-09-03 23:24:46 +0530

    Moharrir all layout
    Moharrir all layout

--- moharrir.php
+++ moharrir.php
@@ -1,33 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-  <title>Bootstrap Example</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+    <title>Welcome Moharrir</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="moharrir.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
 </head>
+
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-        
-        <a class="navbar-brand" href="https://uttarakhandpolice.uk.gov.in/"><img src="Uttarakhand_Police_Logo.png" alt="Smart malkhana image" width="30" height="30"> </a>
-       
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+        <a class="navbar-brand" href="https://uttarakhandpolice.uk.gov.in/"><img src="Uttarakhand_Police_Logo.png"
+                alt="Smart malkhana image" width="30" height="30"> </a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
         <a class="navbar-brand" href="index.php">E-MALKHANA UTTARAKHAND</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
         </button>
-      
+
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav ml-auto">
-            <li class="nav-item active">
-              <a class="nav-link" href="logout.php">LOGOUT<span class="sr-only">(current)</span></a>
-            </li>
-            <!-- <li class="nav-item">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="logout.php">LOGOUT<span class="sr-only">(current)</span></a>
+                </li>
+                <!-- <li class="nav-item">
               <a class="nav-link" href="about.php">ABOUT</a>
             </li>
             <li class="nav-item ">
@@ -37,48 +43,168 @@
               <a class="nav-link" href="privacy.php">PRIVACY</a>
             </li> -->
             </ul>
-    <!-- <form class="form-inline my-2 my-lg-0">
+            <!-- <form class="form-inline my-2 my-lg-0">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form> -->
-  </div>  
-</nav>
+        </div>
+    </nav>
 
 
 
- 
-<div class="container">
-  <!-- <h2>Cards with Contextual Classes</h2> -->
-  <div class="card bg-info text-white">
-    <div class="card-body">Scan new item</div>
-  </div>
-  <br>
-  <div class="card bg-warning text-white">
-    <div class="card-body">Scan old item</div>
-  </div>
-  <br>
-  <div class="card bg-warning text-white">
-    <div class="card-body">Generate QR code for new items</div>
-  </div>
-  <br>
-  <div class="card bg-warning text-white">
-    <div class="card-body">Search for evidence in Malkhana</div>
-  </div>
-  <br>
-  <div class="card bg-warning text-white">
-    <div class="card-body">Track any Evidence</div>
-  </div>
-  <br>
-  <div class="card bg-warning text-white">
-    <div class="card-body">New Disposed item</div>
-  </div>
-  <br>
-  <div class="card bg-warning text-white">
-    <div class="card-body">Already disposed item</div>
-  </div>
-  <br>
-  
-</div>
+
+
+    <!-- all item buttons -->
+
+    <div class="container">
+
+
+
+
+        <div class="row">
+
+            <div class="col-sm-2 border-right">
+                <div class="d-flex flex-column align-items-center text-center p-3 py-5">
+                    <img class="rounded-circle mt-5" width="150px" src="moharrir.png">
+                    <span class="font-weight-bold">Mr. Ayushmaan<br>Moharrir<br>Naintal</span>
+                    <span class="text-black-50"><a href="https://google.com">UPDATE PROFILE</a></span>
+                </div>
+            </div>
+            <div class="col-sm-8">
+
+
+
+
+
+
+
+
+
+                <!-- DS SUCKCXSKV -->
+
+
+
+
+                <!-- <h2>Cards with Contextual Classes</h2> -->
+                <div class="card bg-info text-white">
+
+                    <button type="button" class="button">
+                        <span class="button__icon">
+                            <ion-icon name="scan-outline"></ion-icon>
+                        </span>
+                        <span class="button__text">Scan New Item</span>
+
+                    </button>
+                    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+
+                </div>
+                <br>
+                <div class="card bg-info text-white">
+
+                    <button type="button" class="button2">
+                        <span class="button__icon">
+                            <ion-icon name="scan-outline"></ion-icon>
+                        </span>
+                        <span class="button__text">Scan OLD Item</span>
+
+                    </button>
+                    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+
+                </div>
+                <br>
+                <div class="card bg-warning text-white">
+
+                    <button type="button" class="button2">
+                        <span class="button__icon">
+                            <ion-icon name="qr-code-outline"></ion-icon>
+                        </span>
+                        <span class="button__text">Generate QR code for New Item</span>
+
+                    </button>
+                    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+
+
+
+                </div>
+                <br>
+                <div class="card bg-warning text-white">
+
+
+                    <button type="button" class="button2">
+                        <span class="button__icon">
+                            <ion-icon name="search-outline"></ion-icon>
+                        </span>
+                        <span class="button__text">Search for evidence in Malkhana</span>
+
+                    </button>
+                    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+
+                </div>
+                <br>
+                <div class="card bg-warning text-white">
+
+
+
+                    <button type="button" class="button2">
+                        <span class="button__icon">
+                            <ion-icon name="location-outline"></ion-icon>
+                        </span>
+                        <span class="button__text">Track any Evidence</span>
+
+                    </button>
+                    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+
+
+                </div>
+                <br>
+                <div class="card bg-warning text-white">
+
+
+                    <button type="button" class="button2">
+                        <span class="button__icon">
+                            <ion-icon name="trash-outline"></ion-icon>
+                        </span>
+                        <span class="button__text">New Disposed item</span>
+
+                    </button>
+                    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+
+
+                </div>
+                <br>
+                <div class="card bg-warning text-white">
+
+                    <button type="button" class="button2">
+                        <span class="button__icon">
+                            <ion-icon name="trash-bin-outline"></ion-icon>
+                        </span>
+                        <span class="button__text">Already disposed item</span>
+
+                    </button>
+
+                    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+
+                </div>
+                <br>
+
+
+                <div class="card bg-warning text-white">
+
+                    <button type="button" class="button2">
+                        <span class="button__icon">
+                            <ion-icon name="time-outline"></ion-icon>
+                        </span>
+                        <span class="button__text">History</span>
+
+                    </button>
+
+                    <script src="https://unpkg.com/ionicons@5.4.0/dist/ionicons.js"></script>
+
+                </div>
+                <br>
+            </div>
+        </div>
 
 </body>
+
 </html>
